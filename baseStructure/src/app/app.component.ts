f38bab0f0ba625965fa7998a1ab5ca3325e21f56
jose gomez <josegomez120@gmail.com>
2023-01-28 10:53:37 -0500

    se agrego nuevo titulo y se borro el anterior inicio

--- baseStructure/src/app/app.component.ts
+++ baseStructure/src/app/app.component.ts
@@ -6,5 +6,5 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'baseStructure';
+  title = 'Base Structure for new project';
 }
